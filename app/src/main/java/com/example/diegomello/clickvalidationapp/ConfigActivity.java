c40642b5ebca19ad464da9de528367e863c69f5e
xxporkoxx <diegomello@tokenlab.com.br>
2017-05-30 18:27:26 +0100

    first Commit

---
+++ app/src/main/java/com/example/diegomello/clickvalidationapp/ConfigActivity.java
@@ -0,0 +1,18 @@
+package com.example.diegomello.clickvalidationapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by DiegoMello on 5/30/2017.
+ */
+
+public class ConfigActivity extends AppCompatActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_config);
+    }
+
+
+}
